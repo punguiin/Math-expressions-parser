e5b2e181408fc22ebea3ebe047b83f3f7b2fa718
Nazar Sydorchuk <n.sydorchuk@newtonideas.com>
2025-11-12 13:04:22 +0200

    added credits

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,29 @@ use std::path::PathBuf;
 fn main() -> anyhow::Result<()> {
     let matches = Command::new("Math Expression parser")
         .version("1.0")
+        .author("Nazar Sydorchuk <n.sydorchuk@ukma.edu.ua>")
         .about("Parses and evaluates mathematical expressions from a file")
         .arg(
             Arg::new("file")
                 .short('f')
                 .long("file")
                 .help("Path to the input file containing the mathematical expression")
-                .required(true)
                 .value_parser(PathBufValueParser::new()),
         )
+        .arg(
+            Arg::new("credits")
+                .short('c')
+                .long("credits")
+                .help("Show credits and exit")
+                .action(clap::ArgAction::SetTrue),
+        )
         .get_matches();
+
+    if matches.get_flag("credits") {
+        println!("Math Expression parser v1.0");
+        println!("Author: Nazar Sydorchuk <n.sydorchuk@ukma.edu.ua>");
+        return Ok(());
+    }
     let file_path: &PathBuf = matches.get_one("file").expect("No file path provided");
     let input = std::fs::read_to_string(file_path)?;
     let lines: Vec<&str> = input.lines().collect();
